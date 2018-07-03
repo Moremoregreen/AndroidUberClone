e0d285038c17b4fa42a3dd0e72bbc4d50f8497fe
Moremoregreen <zzz2009j@gmail.com>
2018-07-03 19:01:04 +0800

    登入頁面設計

---
+++ app/src/main/java/com/moremoregreen/androiduberclone/MainActivity.java
@@ -0,0 +1,32 @@
+package com.moremoregreen.androiduberclone;
+//Add lib to use
+//FireBase Authentication
+//FireBase Database
+//Support Design
+//Material Edit Text
+//Calligraphy(客製化文字跟View)
+import android.app.Activity;
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //要在setContentView之前
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath("fonts/Arkhip_font.ttf")
+                .setFontAttrId(R.attr.fontPath)
+                .build());
+        setContentView(R.layout.activity_main);
+    }
+}
